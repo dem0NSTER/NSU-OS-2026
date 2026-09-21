5317ba0b0123e7ef0177d57a3ef0a3ce2ff7e02a
Pavel Fedorenko <p.fedorenko1@g.nsu.ru>
2026-09-21 08:35:57 +0700

    init

---
+++ 25214/p.fedorenko1/lab2/ex_time.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <time.h>
+
+extern char *tzname[];
+
+int main() {
+    time_t now; 
+    struct tm *sp;
+    char timezone[] = "TZ=PST8";
+
+    if (putenv(timezone) != 0) { 
+        perror("putenv");
+        return 1;
+    }
+
+    // tzset();
+    
+    if (time(&now) == (time_t)-1) { 
+        perror("time");
+        return 1;
+    }
+    
+    printf("%s", ctime(&now));
+    
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n", sp->tm_mon + 1, sp->tm_mday, sp->tm_year,
+            sp->tm_hour, sp->tm_min, tzname[sp->tm_isdst]);
+    
+    return 0;
+}
